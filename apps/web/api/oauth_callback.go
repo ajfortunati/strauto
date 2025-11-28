721931d3b169b9ac382825e35ec3afc220bd81ac
anthony fortunati <anthonyjfortunati@gmail.com>
2025-11-27 23:02:30 -0800

    how about return type

--- apps/web/api/oauth_callback.go
+++ apps/web/api/oauth_callback.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 )
 
-func OauthCallback(w http.ResponseWriter, r *http.Request) error {
+func OauthCallback(w http.ResponseWriter, r *http.Request) {
 	// parse code from Strava
 	code := r.URL.Query().Get("code")
 	if code == "" {
 		http.Error(w, "missing code", http.StatusBadRequest)
-		return nil
+		// return nil
 	}
 	// exchange with Strava for tokens
 	fmt.Fprintf(w, "Oauth callback received code: %s\n", code)
-	return nil
+	// return nil
 }
